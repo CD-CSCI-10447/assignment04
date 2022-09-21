a5bf71bbc77a6c74ca2b515cbf7a24810c615f54
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2022-09-21 16:25:14 -0400

    first commit

---
+++ src/main/java/edu/desu/partB/demos/ArrayIndexDemo.java
@@ -0,0 +1,12 @@
+package edu.desu.partB.demos;
+
+public class ArrayIndexDemo {
+
+    public static void main(String args[])
+    {
+        int[] nums = {1, 2, 3};
+        nums[3]=10;
+
+    }
+
+}
